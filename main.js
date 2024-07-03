4e33cfd6ac884b5c5f382a859a7fb34bd77a5dd2
KernelOS <romiluna04@gmail.com>
2024-07-02 21:37:51 -0300

    Agrego funcion para cargar URL e inizializador

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Selector de elementos
 const $ = (id) => document.getElementById(id)
 
 // boton para descargar meme
@@ -8,3 +9,19 @@ const descargarMeme = () => {
 }
 // fin boton descarga 
 
+//Este es para la url de imagen
+const actualizarImagen = (evento) => {
+  if (evento.target.value.length !== 0) {
+    $('image-meme').style.backgroundImage = `url("${evento.target.value}")`
+  }
+}
+//Este es el inicializador de evento de URL
+const inicializarImagen = () => {
+  $('url-img-input').addEventListener('input', actualizarImagen);
+};
+// Inicializar todos los eventos
+const inicializar = () => {
+  inicializarImagen()
+  $('download-meme-button').addEventListener('click', descargarMeme)
+}
+window.onload = inicializar
