cb846107319d31962d5356234e5dbfa842825c8b
KernelOS <romiluna04@gmail.com>
2024-07-03 00:15:42 -0300

    agrego panel img fondo y js que actualiza color

--- main.js
+++ main.js
@@ -9,16 +9,37 @@ const descargarMeme = () => {
 }
 // fin boton descarga 
 
-//Este es para la url de imagen
+//Funcion para la url de imagen
 const actualizarImagen = (evento) => {
   if (evento.target.value.length !== 0) {
     $('image-meme').style.backgroundImage = `url("${evento.target.value}")`
   }
 }
-//Este es el inicializador de evento de URL
+//Funcion para actualizar el color 
+const actualizarColorMezcla = (evento) => {
+  $('blend-mode-color').innerText = evento.target.value.toUpperCase()// Actualiza el texto del color mostrado
+  $('image-meme').style.backgroundColor = evento.target.value // Aplica el color de fondo al elemento 'image-meme'
+}
+// este es para actualizar el tipo de mezcla
+const actualizarTipoMezcla = (evento) => {
+  $('image-meme').style.backgroundBlendMode = evento.target.value // Aplica el modo de mezcla al elemento 'image-meme'
+}
+
+//Este es el inicializador de evento 
 const inicializarImagen = () => {
   $('url-img-input').addEventListener('input', actualizarImagen);
+
+  $('blend-mode-color-input').addEventListener('input', actualizarColorMezcla) //Inicializa la actualización de color de mezcla
+  $('blend-mode-select').addEventListener('change', actualizarTipoMezcla) //Inicializa la actualización de tipo de mezcla
+
+
 };
+
+
+
+
+
+
 // Inicializar todos los eventos
 const inicializar = () => {
   inicializarImagen()
