a36edfb7eb53906c6943171f5a1c431861b13ef7
KernelOS <romiluna04@gmail.com>
2024-07-08 14:17:56 -0300

    Agrego panel de filtros con su js

--- main.js
+++ main.js
@@ -24,6 +24,36 @@ const actualizarColorMezcla = (evento) => {
 const actualizarTipoMezcla = (evento) => {
   $('image-meme').style.backgroundBlendMode = evento.target.value // Aplica el modo de mezcla al elemento 'image-meme'
 }
+//esta funcion es para los filtros
+const actualizarFiltros = () => {
+  const brightness = $('brightness-slider').value
+  const opacity = $('opacity-slider').value
+  const blur = $('blur-slider').value
+  const contrast = $('contrast-slider').value
+  const grayscale = $('grayscale-slider').value
+  const hue = $('hue-slider').value
+  const sepia = $('sepia-slider').value
+  const saturate = $('saturate-slider').value
+  const invert = $('invert-slider').value
+
+  $(
+    'image-meme'
+  ).style.filter = `brightness(${brightness}) opacity(${opacity}) blur(${blur}px) contrast(${contrast}%) grayscale(${grayscale}%) hue-rotate(${hue}deg) sepia(${sepia}%) saturate(${saturate}%) invert(${invert})`
+}
+//Funcion para reestablecer los filtros
+const reestrablecerFiltros = () => {
+  $('brightness-slider').value = 1
+  $('opacity-slider').value = 1
+  $('blur-slider').value = 0
+  $('contrast-slider').value = 100
+  $('grayscale-slider').value = 0
+  $('hue-slider').value = 0
+  $('sepia-slider').value = 0
+  $('saturate-slider').value = 100
+  $('invert-slider').value = 0
+
+  actualizarFiltros()
+}
 
 //Este es el inicializador de evento 
 const inicializarImagen = () => {
@@ -32,12 +62,18 @@ const inicializarImagen = () => {
   $('blend-mode-color-input').addEventListener('input', actualizarColorMezcla) //Inicializa la actualización de color de mezcla
   $('blend-mode-select').addEventListener('change', actualizarTipoMezcla) //Inicializa la actualización de tipo de mezcla
 
+  $('brightness-slider').addEventListener('change', actualizarFiltros) // aca arranca inicializacion para filtros
+  $('opacity-slider').addEventListener('change', actualizarFiltros)
+  $('blur-slider').addEventListener('change', actualizarFiltros)
+  $('contrast-slider').addEventListener('change', actualizarFiltros)
+  $('grayscale-slider').addEventListener('change', actualizarFiltros)
+  $('hue-slider').addEventListener('change', actualizarFiltros)
+  $('sepia-slider').addEventListener('change', actualizarFiltros)
+  $('saturate-slider').addEventListener('change', actualizarFiltros)
+  $('invert-slider').addEventListener('change', actualizarFiltros)
 
+  $('default-filters-button').addEventListener('click', reestrablecerFiltros) //inicializa la funcion que Reestablece los filtros.
 };
-
-
-
-
 
 
 // Inicializar todos los eventos
